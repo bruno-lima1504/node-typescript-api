9f82127105056d7b9c26dee3e09d1d8ad560fe67
dev-admin01 <bruno.lima@bslsoftware.com.br>
2026-04-02 11:56:10 -0300

    test: finalize the e2e test setup and add

--- src/util/module-alias.ts
+++ src/util/module-alias.ts
@@ -4,6 +4,6 @@ import * as moduleAlias from "module-alias";
 const files = path.resolve(__dirname, "../..");
 
 moduleAlias.addAliases({
-  "@": path.join(files, "src"),
+  "@src": path.join(files, "src"),
   "@test": path.join(files, "test"),
 });
